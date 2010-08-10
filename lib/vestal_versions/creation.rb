bc701ff20d7b233ebba75e9c09f6eb394a5f1934
Jordan Bach <jordan.bach@reelfx.com>
2010-08-10 09:46:13 -0500

    renamed versions.changes column to version.vchanges to avoid conflicts with ActiveRecord::Base.changes

--- lib/vestal_versions/creation.rb
+++ lib/vestal_versions/creation.rb
@@ -55,8 +55,8 @@ module VestalVersions
         # Updates the last version's changes by appending the current version changes.
         def update_version
           return create_version unless v = versions.last
-          v.changes_will_change!
-          v.update_attribute(:changes, v.changes.append_changes(version_changes))
+          v.vchanges_will_change!
+          v.update_attribute(:vchanges, v.vchanges.append_changes(version_changes))
           reset_version_changes
           reset_version
         end
@@ -78,7 +78,7 @@ module VestalVersions
         # Specifies the attributes used during version creation. This is separated into its own
         # method so that it can be overridden by the VestalVersions::Users feature.
         def version_attributes
-          {:changes => version_changes, :number => last_version + 1}
+          {:vchanges => version_changes, :number => last_version + 1}
         end
     end
   end
--- lib/vestal_versions/version.rb
+++ lib/vestal_versions/version.rb
@@ -6,12 +6,9 @@ module VestalVersions
     # Associate polymorphically with the parent record.
     belongs_to :versioned, :polymorphic => true
 
-    # ActiveRecord::Base#changes is an existing method, so before serializing the +changes+ column,
-    # the existing +changes+ method is undefined. The overridden +changes+ method pertained to 
-    # dirty attributes, but will not affect the partial updates functionality as that's based on
-    # an underlying +changed_attributes+ method, not +changes+ itself.
-    undef_method :changes
-    serialize :changes, Hash
+    # A column cannot be named changes for an active record model because it conflcits with
+    # ActiveRecord::Base#changes included from ActiveRecord::Dirty
+    serialize :vchanges, Hash
 
     # In conjunction with the included Comparable module, allows comparison of version records
     # based on their corresponding version numbers, creation timestamps and IDs.
